244db2ebd5cdcdbd717d279586f66ee34cd44ead
Dylan Steele <dylansteele57@gmail.com>
2023-12-15 21:26:39 -0500

    fixed bash defaults for params

--- build-deploy.sh
+++ build-deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
-copy_path="$1" || "/var/www/html/dsteele.dev/"
+DEFAULTVALUE="/var/www/html/dsteele.dev"
+copy_path="${1:-$DEFAULTVALUE}"
 
 # Build and deploy the jekyll site assets
 docker build -t jekyll-builder:latest -f JekyllDockerfile .
 
 docker create -ti --name dummy jekyll-builder:latest bash
-docker cp dummy:/tmp/_site/. $copy_path
+sudo docker cp dummy:/tmp/_site/. $copy_path
 docker rm -f dummy
